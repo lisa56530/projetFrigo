a8f88a4cea1661b5df4fa02e8a061b3fbab4b2f4
lisa56530 <lisacapitaine@icloud.com>
2024-02-22 22:37:52 +0100

    cjhishvc

--- src/components/FrigoOuvert.vue
+++ src/components/FrigoOuvert.vue
@@ -39,7 +39,7 @@ const url = "https://webmmi.iut-tlse3.fr/~pecatte/frigo/public/8/produits";
       </ul>
       
       <br> <br>
-      <img src= "src\assets\FrigoNourritures.jpg" >
+      <img src= './FrigoNourritures.jpg' alt="frigo" >
 
 
 
